5ae77fdf66bf7a17fc7acf8ff06febf60d1e4e41
ngodi <albertngodi@gmail.com>
2019-08-13 00:03:49 +0100

    event create action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  
+
   def new
     @event = Event.new
   end
@@ -11,5 +11,18 @@ class EventsController < ApplicationController
   end
 
   def create
+    current_user
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to current_user
+    else
+      redirect_to 'new'
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:description, :date)
   end
 end
