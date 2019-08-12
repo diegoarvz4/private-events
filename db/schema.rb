6cfc8f29eb5ed90600b3e1a1d7994309065d54e1
diegoarvz4 <darvz.gtz@gmail.com>
2019-08-12 15:42:11 -0500

    associations between events and creators

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190812172238) do
+ActiveRecord::Schema.define(version: 20190812184923) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "description"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
