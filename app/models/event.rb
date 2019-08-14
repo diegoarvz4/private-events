c0c1df727ff5aa3924033c9341d0b1a22205135c
diegoarvz4 <darvz.gtz@gmail.com>
2019-08-14 00:53:24 -0500

    associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
 
    belongs_to :creator, class_name: 'User'
+   has_many :event_attendings, foreign_key: :attended_event_id
    has_many :attendees, through: :event_attendings, source: :event_attendee
 
 end
